235c2ba2d444fe634364b894654e3aef64668d39
Thirafi Wian <thirafi.badai@gmail.com>
2018-01-27 08:14:18 +0700

    Parsing V1
    more practice again, dan saya skip kelas :v

---
+++ tes/tes/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace tes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConn = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnDiscc = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "300",
+            "500",
+            "9600",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(167, 28);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baud Rate";
+            // 
+            // btnConn
+            // 
+            this.btnConn.Location = new System.Drawing.Point(21, 55);
+            this.btnConn.Name = "btnConn";
+            this.btnConn.Size = new System.Drawing.Size(75, 23);
+            this.btnConn.TabIndex = 4;
+            this.btnConn.Text = "Connect";
+            this.btnConn.UseVisualStyleBackColor = true;
+            this.btnConn.Click += new System.EventHandler(this.btnConn_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(113, 55);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnDiscc
+            // 
+            this.btnDiscc.Location = new System.Drawing.Point(194, 55);
+            this.btnDiscc.Name = "btnDiscc";
+            this.btnDiscc.Size = new System.Drawing.Size(75, 23);
+            this.btnDiscc.TabIndex = 6;
+            this.btnDiscc.Text = "Disconnect";
+            this.btnDiscc.UseVisualStyleBackColor = true;
+            this.btnDiscc.Click += new System.EventHandler(this.btnDiscc_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(31, 97);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(74, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(31, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(74, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(204, 149);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(74, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(204, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(74, 20);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(204, 97);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(74, 20);
+            this.textBox7.TabIndex = 17;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(31, 272);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(74, 20);
+            this.textBox8.TabIndex = 22;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(31, 246);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(74, 20);
+            this.textBox9.TabIndex = 21;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(31, 220);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(74, 20);
+            this.textBox10.TabIndex = 20;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(204, 272);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(74, 20);
+            this.textBox11.TabIndex = 25;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(204, 246);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(74, 20);
+            this.textBox12.TabIndex = 24;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(204, 220);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(74, 20);
+            this.textBox13.TabIndex = 23;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(332, 264);
+            this.richTextBox1.TabIndex = 26;
+            this.richTextBox1.Text = "";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 312);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.btnDiscc);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConn;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnDiscc;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ tes/tes/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace tes
+{
+    public partial class Form1 : Form
+    {
+
+        string kata;
+        string[] words;
+
+        //int xTimeStamp = 1;
+        string header, ax, ay, az, gx, gy, gz, suhu, lembab, altid, latid, longit; //inisiasi string dari dummies
+
+        private void serialPort1_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            kata = serialPort1.ReadLine();
+            words = Regex.Split(kata, " ");
+            //take the information from dummies to richTextBox
+            if (words[0] == "005" && words.Length == 13)
+                richTextBox1.BeginInvoke(new myDelegate(updateTextBox));
+
+        }
+
+        public delegate void myDelegate();
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            kata = serialPort1.ReadLine();
+            words = Regex.Split(kata, " ");
+
+            if (words[0] == "005" && words.Length == 13)
+                richTextBox1.BeginInvoke(new myDelegate(updateTextBox));
+        }
+
+        public void updateTextBox()
+        {
+            //taking each string and put them to arrays
+            richTextBox1.Text = kata;
+            header = words[0];
+            ax = words[1];
+            ay = words[2];
+            az = words[3];
+            gx = words[4];
+            gy = words[5];
+            gz = words[6];
+            suhu = words[7];
+            lembab = words[8];
+            altid = words[9];
+            latid = words[10];
+            longit = words[11];
+            richTextBox1.Text = words[0];
+            richTextBox1.AppendText(kata);
+            //richTextBox2.Text = words[1];
+            //fetch data from arrayas before
+            textBox6.Text = ax;
+            textBox2.Text = ay;
+            textBox3.Text = az;
+            textBox7.Text = gx;
+            textBox5.Text = gy;
+            textBox4.Text = gz;
+            textBox10.Text = suhu;
+            textBox9.Text = lembab;
+            textBox8.Text = altid;
+            textBox13.Text = latid;
+            textBox12.Text = longit;
+            textBox11.Text = ("mantap");
+            
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            //fetching port name from seriapPort1
+            string[] port = SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+            foreach (String sambung in port)
+            {
+                comboBox1.Items.Add(sambung);
+            }
+        }
+
+        private void btnConn_Click(object sender, EventArgs e)
+        {
+            //6 holy lines to conn
+            serialPort1.PortName = comboBox1.Text;
+            serialPort1.BaudRate = int.Parse(comboBox2.Text);
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DataBits = 8;
+            serialPort1.Open();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("1");
+        }
+
+        private void btnDiscc_Click(object sender, EventArgs e)
+        {
+            serialPort1.Dispose();
+            serialPort1.Close();
+
+            btnConn.Enabled = true;
+            btnDiscc.Enabled = true;
+        }
+    }
+}
