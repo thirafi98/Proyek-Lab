0a21fe560219209d563b85d93ab9b088492e1b92
Thirafi Wian <thirafi.badai@gmail.com>
2018-01-31 01:54:42 +0700

    Pharsing V3 (with avionic)
    au ah

---
+++ parsing project/parsing project/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace parsing_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.airSpeedIndicatorInstrumentControl1 = new parsing_project.AirSpeedIndicatorInstrumentControl();
+            this.altimeterInstrumentControl1 = new parsing_project.AltimeterInstrumentControl();
+            this.attitudeIndicatorInstrumentControl1 = new parsing_project.AttitudeIndicatorInstrumentControl();
+            this.headingIndicatorInstrumentControl1 = new parsing_project.HeadingIndicatorInstrumentControl();
+            this.instrumentControl1 = new parsing_project.InstrumentControl();
+            this.turnCoordinatorInstrumentControl1 = new parsing_project.TurnCoordinatorInstrumentControl();
+            this.verticalSpeedIndicatorInstrumentControl1 = new parsing_project.VerticalSpeedIndicatorInstrumentControl();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(139, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(266, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(163, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(163, 167);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(163, 193);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(321, 140);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 11;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(321, 167);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(321, 193);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 13;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(128, 229);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 15;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(241, 229);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 16;
+            // 
+            // MainMap
+            // 
+            this.MainMap.Bearing = 0F;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MainMap.LevelsKeepInMemmory = 5;
+            this.MainMap.Location = new System.Drawing.Point(427, 140);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 22;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.NegativeMode = false;
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(503, 239);
+            this.MainMap.TabIndex = 21;
+            this.MainMap.Zoom = 18D;
+            this.MainMap.Load += new System.EventHandler(this.Form1_Load);
+            // 
+            // airSpeedIndicatorInstrumentControl1
+            // 
+            this.airSpeedIndicatorInstrumentControl1.Location = new System.Drawing.Point(428, 12);
+            this.airSpeedIndicatorInstrumentControl1.Name = "airSpeedIndicatorInstrumentControl1";
+            this.airSpeedIndicatorInstrumentControl1.Size = new System.Drawing.Size(75, 76);
+            this.airSpeedIndicatorInstrumentControl1.TabIndex = 22;
+            this.airSpeedIndicatorInstrumentControl1.Text = "airSpeedIndicatorInstrumentControl1";
+            // 
+            // altimeterInstrumentControl1
+            // 
+            this.altimeterInstrumentControl1.Location = new System.Drawing.Point(534, 12);
+            this.altimeterInstrumentControl1.Name = "altimeterInstrumentControl1";
+            this.altimeterInstrumentControl1.Size = new System.Drawing.Size(75, 76);
+            this.altimeterInstrumentControl1.TabIndex = 23;
+            this.altimeterInstrumentControl1.Text = "altimeterInstrumentControl1";
+            // 
+            // attitudeIndicatorInstrumentControl1
+            // 
+            this.attitudeIndicatorInstrumentControl1.Location = new System.Drawing.Point(636, 10);
+            this.attitudeIndicatorInstrumentControl1.Name = "attitudeIndicatorInstrumentControl1";
+            this.attitudeIndicatorInstrumentControl1.Size = new System.Drawing.Size(75, 76);
+            this.attitudeIndicatorInstrumentControl1.TabIndex = 24;
+            this.attitudeIndicatorInstrumentControl1.Text = "attitudeIndicatorInstrumentControl1";
+            // 
+            // headingIndicatorInstrumentControl1
+            // 
+            this.headingIndicatorInstrumentControl1.Location = new System.Drawing.Point(732, 12);
+            this.headingIndicatorInstrumentControl1.Name = "headingIndicatorInstrumentControl1";
+            this.headingIndicatorInstrumentControl1.Size = new System.Drawing.Size(75, 76);
+            this.headingIndicatorInstrumentControl1.TabIndex = 25;
+            this.headingIndicatorInstrumentControl1.Text = "headingIndicatorInstrumentControl1";
+            // 
+            // instrumentControl1
+            // 
+            this.instrumentControl1.Location = new System.Drawing.Point(813, 12);
+            this.instrumentControl1.Name = "instrumentControl1";
+            this.instrumentControl1.Size = new System.Drawing.Size(75, 81);
+            this.instrumentControl1.TabIndex = 26;
+            this.instrumentControl1.Text = "instrumentControl1";
+            // 
+            // turnCoordinatorInstrumentControl1
+            // 
+            this.turnCoordinatorInstrumentControl1.Location = new System.Drawing.Point(37, 289);
+            this.turnCoordinatorInstrumentControl1.Name = "turnCoordinatorInstrumentControl1";
+            this.turnCoordinatorInstrumentControl1.Size = new System.Drawing.Size(75, 74);
+            this.turnCoordinatorInstrumentControl1.TabIndex = 27;
+            this.turnCoordinatorInstrumentControl1.Text = "turnCoordinatorInstrumentControl1";
+            // 
+            // verticalSpeedIndicatorInstrumentControl1
+            // 
+            this.verticalSpeedIndicatorInstrumentControl1.Location = new System.Drawing.Point(119, 289);
+            this.verticalSpeedIndicatorInstrumentControl1.Name = "verticalSpeedIndicatorInstrumentControl1";
+            this.verticalSpeedIndicatorInstrumentControl1.Size = new System.Drawing.Size(75, 74);
+            this.verticalSpeedIndicatorInstrumentControl1.TabIndex = 28;
+            this.verticalSpeedIndicatorInstrumentControl1.Text = "verticalSpeedIndicatorInstrumentControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 399);
+            this.Controls.Add(this.verticalSpeedIndicatorInstrumentControl1);
+            this.Controls.Add(this.turnCoordinatorInstrumentControl1);
+            this.Controls.Add(this.instrumentControl1);
+            this.Controls.Add(this.headingIndicatorInstrumentControl1);
+            this.Controls.Add(this.attitudeIndicatorInstrumentControl1);
+            this.Controls.Add(this.altimeterInstrumentControl1);
+            this.Controls.Add(this.airSpeedIndicatorInstrumentControl1);
+            this.Controls.Add(this.MainMap);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private AirSpeedIndicatorInstrumentControl airSpeedIndicatorInstrumentControl1;
+        private AltimeterInstrumentControl altimeterInstrumentControl1;
+        private AttitudeIndicatorInstrumentControl attitudeIndicatorInstrumentControl1;
+        private HeadingIndicatorInstrumentControl headingIndicatorInstrumentControl1;
+        private InstrumentControl instrumentControl1;
+        private TurnCoordinatorInstrumentControl turnCoordinatorInstrumentControl1;
+        private VerticalSpeedIndicatorInstrumentControl verticalSpeedIndicatorInstrumentControl1;
+    }
+}
+
---
+++ parsing project/parsing project/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+//using System.IO.Ports.SerialPort;
+using System.IO;
+using System.Text.RegularExpressions;
+using PCComm;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using GMap.NET.Projections;
+
+
+
+namespace parsing_project
+{
+    public partial class Form1 : Form
+    {
+
+        public delegate void myDelegate();
+
+        private SerialPort comPort = new SerialPort();
+
+       // SerialPort port();
+
+        CommunicationManager comm = new CommunicationManager();
+        CommunicationManager commAT = new CommunicationManager();
+        static double xTimeStamp = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comm.isOpen() == true) // Kalau Kebuka Serial Portnya, ditutup dulu 
+            {
+                //button1.Text = "Connect";
+                System.Threading.Thread.Sleep(100);
+                comm.ClosePort();
+            }
+            else
+            {
+                /*serialPort1.PortName = comboBox1.Text; // PIlih Port Koneksi
+                serialPort1.BaudRate = int.Parse(comboBox2.Text); // Pilih Baudrate
+                serialPort1.Parity = Parity.None; // Parity 1, Databits 8, Stopbits 1
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.Open();*/
+
+                if (comboBox1.Text == "") { return; }
+                comm.Parity = "None";
+                comm.StopBits = "One";
+                comm.DataBits = "8";
+                comm.BaudRate = comboBox2.Text;
+                comm.DisplayWindow = richTextBox1;
+               // comm.DisplayWindowforGambar = richTextBox2;
+                //comm.DisplayGambar = pictureBox1;
+                comm.PortName = comboBox1.Text;
+                comm.OpenPort();
+
+                
+                //button1.Text = "Disconnect";
+
+                //timer2.Start();
+                button1.Enabled = false;
+                button2.Enabled = true;
+               // textBox4.Text = comboBox1.Text + " conneected";
+            }
+            /*catch
+            {
+                MessageBox.Show("Failed to Connect "+ comboBox1.Text);
+            }*/
+        }
+
+        public void DiscardOutBuffer()
+        {
+
+        }
+
+        public void DiscardInBuffer()
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //timerr.Start();
+            comm.WriteData("1");
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+            timer1.Start();
+            DiscardInBuffer();
+            comm.gambar = 0;
+            timer1.Tick += new EventHandler(FastTimer);
+          
+        }
+
+
+       /* private async void ClearBuffer()
+        {
+            try
+            {
+                DataReader reader = soc.GetReaderStream();
+                reader.InputStreamOptions = InputStreamOptions.Partial;
+                uint count = reader.UnconsumedBufferLength;
+
+                //if (count > 0)
+                {
+                    // TODO Buffer overflow exception
+                    await reader.LoadAsync(count);
+                    reader.ReadBuffer(count);
+                }
+            }
+        } 
+        */
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+
+            string[] port = SerialPort.GetPortNames();
+
+
+            comboBox1.Items.Clear();
+
+            foreach (String sambung in port)
+            {
+                comboBox1.Items.Add(sambung);
+            }
+        }
+
+        int yaws, air, head;
+        string lala, header, a, b,c,d,e,f,g,h,i, j, k,l,m, line;
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        double roll, pitch, yaw, suhu, alti, tekan, bearing = 000.0;
+        float x, y, z, r, p ;
+        string[] data,klo;// = new string[15];
+
+        private void FastTimer(Object myObject, EventArgs myEventArgs)
+        {
+            string[] tempArray = comm.DisplayWindow.Lines;
+          string line = tempArray[tempArray.Length - 2];
+            if (line == "") return;
+            
+            /*
+            try
+            {
+                line = line.Trim();
+            }
+            catch (IOException ex)
+            {
+                //process any errors
+            }
+            */
+
+           // data = Regex.Split(line, "a");
+            data = Regex.Split(line, " ");
+         //   data = Regex.Split(line, "a");
+            header = data[0];
+            if (comm.gambar == 0)
+            {
+
+                if (header == "005" && data.Length == 13 )
+                {
+                    a = data[1];
+                    b = data[2];
+                    c = data[3];
+                    d = data[4];
+                    e = data[5];
+                    f = data[6];
+                    g = data[7];
+                    
+                    label1.Text = Convert.ToString(line.Length);
+                    //sesuaikan data parsing ke payload
+                    //jangan lupa sesuaikan dengan paramater yang sudah di set di atas dan dibawah
+                    air = int.Parse(data[5]);
+                    pitch = double.Parse(data[7]);
+                    roll = double.Parse(data[6]);
+                    head = int.Parse(data[0]);
+                }
+                
+            }
+
+
+        }
+
+        public void updateData()
+        {
+            //deklarasi instrumen dan sesuaikan bebs
+
+            //airSpeedIndicatorInstrumentControl1.SetAirSpeedIndicatorParameters();
+            altimeterInstrumentControl1.SetAlimeterParameters(air);
+            attitudeIndicatorInstrumentControl1.SetAttitudeIndicatorParameters(pitch,roll);
+            headingIndicatorInstrumentControl1.SetHeadingIndicatorParameters(head);
+            //instrumentControl1
+            //turnCoordinatorInstrumentControl1.SetTurnCoordinatorParameters();
+            //verticalSpeedIndicatorInstrumentControl1.SetVerticalSpeedIndicatorParameters()
+
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(l), Convert.ToDouble(m)), GMarkerGoogleType.green);
+            markersOverlay.Markers.Clear();
+            MainMap.Overlays.Add(markersOverlay);
+            markersOverlay.Markers.Add(marker);
+            MainMap.Invalidate(false);
+
+
+            richTextBox1.Invoke(new EventHandler(delegate
+            {
+                textBox1.Text = a;
+                textBox2.Text = b;
+                textBox3.Text = c;
+                textBox4.Text = d;
+                textBox5.Text = e;
+                textBox6.Text = f;
+                textBox7.Text = g;
+                textBox8.Text = h;
+                textBox9.Text = i;
+
+            }));
+            //richTextBox1.AppendText(line);
+            richTextBox1.ScrollToCaret();
+            xTimeStamp = xTimeStamp + 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MainMap.MapProvider = BingSatelliteMapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = AccessMode.ServerOnly;
+            MainMap.Position = new PointLatLng(-6.9768651, 107.63018883);
+
+            Double lat = -6.9768651;
+            Double lng = 107.63018883;
+
+            //GmarkerGoogle
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
